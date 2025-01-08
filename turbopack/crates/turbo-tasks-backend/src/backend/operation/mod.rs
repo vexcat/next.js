c3f576714c5b31e3950f472bd8e79c554137f7c7
Tobias Koppers <tobias.koppers@googlemail.com>
2025-01-08 16:52:02 +0100

    Revert "split aggregation number update from cleanup/connect"
    This reverts commit c24367cd58b08adacd6b0f6df4e41603345f780d.

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/mod.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/mod.rs
@@ -4,7 +4,6 @@ mod connect_child;
 mod connect_children;
 mod invalidate;
 mod update_cell;
-mod update_children_aggregation;
 mod update_collectible;
 mod update_output;
 
@@ -753,6 +752,5 @@ pub use self::{
     connect_children::connect_children,
     invalidate::TaskDirtyCause,
     update_cell::UpdateCellOperation,
-    update_children_aggregation::update_children_aggregation,
     update_collectible::UpdateCollectibleOperation,
 };
